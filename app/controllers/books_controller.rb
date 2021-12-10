42d3694e1ffa7e444c3398edf7f98619c129dbbb
EC2 Default User <[watawata.20.higr@outlook.jp]>
2021-12-10 11:58:00 +0000

    Topページへのindex作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,29 @@
 class BooksController < ApplicationController
   def index
+
   end
 
   def show
   end
 
   def new
+    @list = List.new
+  end
+
+  def create
+    # １. データを新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # ２. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # ３. トップ画面へリダイレクト
+    redirect_to '/books'
   end
 
   def edit
+  end
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
